9a3433e656affb0770762c3b4dc5e070d489a57c
Vishnu Dileesh <dev@vishnudileesh.com>
2020-08-17 20:51:25 +0530

    Add - middlewares - helmet morgan cors

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
