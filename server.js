fd3970fff83d839de619a056728c226f7ac4d040
Vishnu Dileesh <dev@vishnudileesh.com>
2020-09-02 13:39:02 +0530

    Add eslint - add param validation check in for user registration and login

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-const monk = require("monk");
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
-const morgan = require("morgan");
-const cors = require("cors");
-const yup = require("yup");
-const PORT = process.env.PORT;
+const monk = require('monk');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const morgan = require('morgan');
+const cors = require('cors');
+const yup = require('yup');
+
+const {PORT} = process.env;
 
 // authentication import packages
 
@@ -30,9 +32,7 @@ const saltRounds = 10;
 const dbURI = process.env.DB_URI;
 const db = monk(dbURI);
 
-db.then(() => {
-  console.log("Connected to DB Successfulyy");
-});
+db.then(() => { console.info("Connected to DB Successfulyy"); });
 
 const todo = db.get("todo");
 
@@ -50,8 +50,8 @@ app.use(express.json());
 // rate limiter config
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs : 15 * 60 * 1000, // 15 minutes
+  max : 100,                 // limit each IP to 100 requests per windowMs
 });
 
 app.use(limiter);
@@ -66,14 +66,14 @@ const jwtVerify = (req, res, next) => {
   if (authHeader) {
     const token = authHeader.split(" ")[1];
 
-    jwt.verify(token, tokenSecretKey, (err, user) => {
+    jwt.verify(token, tokenSecretKey, (err, authUser) => {
       if (err) {
         return res.sendStatus(403);
       }
 
-      req.user = user;
+      req.user = authUser;
 
-      next();
+      return next();
     });
   } else {
     res.sendStatus(401);
@@ -84,84 +84,89 @@ const jwtVerify = (req, res, next) => {
 
 // yup - userSchema config
 
-let userSchema = yup.object().shape({
-  username: yup.string().required(),
-  email: yup.string().email().required(),
-  password: yup.string().min(8).required(),
-  joined_on: yup.date().default(() => {
-    return new Date();
-  }),
+const userRegistrationSchema = yup.object().shape({
+  username : yup.string().required(),
+  email : yup.string().email().required(),
+  password : yup.string().min(8).required(),
+  joined_on : yup.date().default(() => { return new Date(); }),
+});
+
+const userLoginSchema = yup.object().shape({
+  email : yup.string().email().required(),
+  password : yup.string().min(8).required(),
 });
 
 // end yup - userSchema config
 
 // start routes
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+app.get("/", (req, res) => { res.send("hello world"); });
 
 // user registration
 
 app.post("/users/register", (req, res) => {
-  const { username, email, password } = req.body;
+  const {username, email, password} = req.body;
 
-  let newUser = {
+  const newUser = {
     username,
     email,
     password,
   };
 
-  userSchema
-    .validate(newUser)
-    .then((validUser) => {
-      bcrypt.hash(validUser.password, saltRounds, (err, result) => {
-        if (result) {
-          validUser.password = result;
+  userRegistrationSchema.validate(newUser)
+      .then(() => {
+        bcrypt.hash(newUser.password, saltRounds, (err, result) => {
+          if (result) {
+            newUser.password = result;
 
-          user
-            .insert(validUser)
-            .then((user) => {
-              console.log(user);
-              res.json({ message: "success" });
-            })
-            .catch((err) => {
-              res.status(500).json({ error: err });
-            });
-        } else {
-          res.sendStatus(500);
-        }
+            user.insert(newUser)
+                .then(
+                    () => { res.status(201).json({message : "success"}); })
+                .catch(() => { res.status(500).json({error : err}); });
+          } else {
+            res.sendStatus(500);
+          }
+        });
+      })
+      .catch((err) => {
+        res.status(422).json({error : err.name, detail : err.errors});
       });
-    })
-    .catch((err) => {
-      res.status(422).json({ error: err.name, detail: err.errors });
-    });
 });
 
 // user login
 
 app.post("/users/login", (req, res) => {
-  const { email, password } = req.body;
+  const {email, password} = req.body;
 
-  user
-    .findOne({ email })
-    .then((user) => {
-      bcrypt.compare(password, user.password).then((result) => {
-        if (result) {
-          const accessToken = jwt.sign(
-            { id: user._id, email: user.email },
-            tokenSecretKey,
-            {
-              expiresIn: "1d",
-            }
-          );
-          res.json(accessToken);
-        } else {
-          res.json({ error: "email or password is incorrect" });
-        }
-      });
+  const newUserLogin = {
+    email,
+    password
+  }
+
+  userLoginSchema.validate(newUserLogin)
+    .then(() => {
+    
+      user.findOne({email})
+        .then((foundUser) => {
+          bcrypt.compare(password, foundUser.password)
+            .then((result) => {
+              if(result){
+                const accessToken = jwt.sign({id: foundUser.id, username: foundUser.username, email: foundUser.email}, tokenSecretKey, {
+                  expiresIn: "1d",
+                });
+
+                res.json(accessToken);
+              }else{
+                res.json({error: "email or password is incorrect"});
+              }
+            });
+        })
+        .catch(() => res.json({error: "email or password is incorrect"})); 
     })
-    .catch((err) => res.json({ error: "email or password is incorrect" }));
+    .catch((err) => {
+      res.status(422).json({error: err.name, details: err.errors});
+    });
+
 });
 
 // get all todo item from database
@@ -169,20 +174,17 @@ app.post("/users/login", (req, res) => {
 app.get("/todos", jwtVerify, (req, res) => {
   const user_id = req.user.id;
 
-  todo
-    .find({ user_id })
-    .then((docs) => {
-      res.json({ data: docs });
-    })
-    .catch((err) => res.json({ error: err }));
+  todo.find({user_id})
+      .then((docs) => { res.json({data : docs}); })
+      .catch((err) => res.json({error : err}));
 });
 
 // add todo item to database
 
 app.post("/todos", jwtVerify, (req, res) => {
-  const { item } = req.body;
+  const {item} = req.body;
 
-  let completed = req.body.completed;
+  let {completed} = req.body;
 
   if (completed === undefined) {
     completed = false;
@@ -190,41 +192,33 @@ app.post("/todos", jwtVerify, (req, res) => {
 
   const user_id = req.user.id;
 
-  let newTodo = {
+  const newTodo = {
     item,
     completed,
     user_id,
-    created_at: new Date(),
+    created_at : new Date(),
   };
 
-  todo
-    .insert(newTodo)
-    .then((doc) => {
-      res.json(doc);
-    })
-    .catch((err) => {
-      res.json({ error: err });
-    });
+  todo.insert(newTodo)
+      .then((doc) => { res.json(doc); })
+      .catch((err) => { res.json({error : err}); });
 });
 
 // get single todo item matching the id
 
 app.get("/todos/:id", jwtVerify, (req, res) => {
-  let todo_id = req.params.id;
+  const todo_id = req.params.id;
 
   const user_id = req.user.id;
 
-  todo
-    .findOne({ _id: todo_id, user_id: user_id })
-    .then((doc) => {
-      if (doc === null) {
-        res.sendStatus(404);
-      }
-      res.json({ data: doc });
-    })
-    .catch((err) => {
-      res.sendStatus(404);
-    });
+  todo.findOne({_id : todo_id, user_id})
+      .then((doc) => {
+        if (doc === null) {
+          res.sendStatus(404);
+        }
+        res.json({data : doc});
+      })
+      .catch((err) => { res.sendStatus(404); });
 });
 
 // update single todo item matching the id
@@ -235,39 +229,31 @@ app.put("/todos/:id", jwtVerify, (req, res) => {
   const user_id = req.user.id;
 
   if (req.body.item === undefined && req.body.completed === undefined) {
-    res.json({ message: "value of item or completed is to be passed" });
+    res.json({message : "value of item or completed is to be passed"});
   }
 
   if (req.body.item) {
-    todo
-      .findOneAndUpdate(
-        { _id: todo_id, user_id: user_id },
-        { $set: { item: req.body.item } }
-      )
-      .then((doc) => {
-        if (doc === null) {
-          res.sendStatus(404);
-        }
-        res.json(doc);
-      })
-      .catch((err) => {
-        res.sendStatus(404);
-      });
+    todo.findOneAndUpdate({_id : todo_id, user_id},
+                          {$set : {item : req.body.item}})
+        .then((doc) => {
+          if (doc === null) {
+            res.sendStatus(404);
+          }
+          res.json(doc);
+        })
+        .catch((err) => { res.sendStatus(404); });
   }
 
   if (req.body.completed) {
-    todo
-      .findOneAndUpdate(
-        { _id: todo_id, user_id: user_id },
-        { $set: { completed: req.body.completed } }
-      )
-      .then((doc) => {
-        if (doc === null) {
-          res.sendStatus(401);
-        }
-        res.json(doc);
-      })
-      .catch((err) => res.json({ error: err }));
+    todo.findOneAndUpdate({_id : todo_id, user_id},
+                          {$set : {completed : req.body.completed}})
+        .then((doc) => {
+          if (doc === null) {
+            res.sendStatus(401);
+          }
+          res.json(doc);
+        })
+        .catch((err) => res.json({error : err}));
   }
 });
 
@@ -278,22 +264,19 @@ app.delete("/todos/:id", jwtVerify, (req, res) => {
 
   const user_id = req.user.id;
 
-  todo
-    .findOneAndDelete({ _id: todo_id, user_id: user_id })
-    .then((doc) => {
-      if (doc === null) {
-        res.sendStatus(404);
-      }
+  todo.findOneAndDelete({_id : todo_id, user_id})
+      .then((doc) => {
+        if (doc === null) {
+          res.sendStatus(404);
+        }
 
-      res.json({ message: "success", action: "todo deleted" });
-    })
-    .catch((err) => res.json({ error: err }));
+        res.json({message : "success", action : "todo deleted"});
+      })
+      .catch((err) => res.json({error : err}));
 });
 
 // end routes
 
 // start server
 
-app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
-});
+app.listen(PORT, () => { console.log(`Server running at port ${PORT}`); });
